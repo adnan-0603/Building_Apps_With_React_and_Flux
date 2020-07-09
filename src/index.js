a5836826db3179f1a9db5d5674a61da23ae998bc
chowdhury.mdadnan@gmail.com <chowdhury.mohammadadnan06@gmail.com>
2020-07-09 19:52:14 +0600

    adding homepage

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import  "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import { render } from "react-dom";
+import Homepage from "./components/HomePage";
+
+render (<Homepage />, document.getElementById("root"));
