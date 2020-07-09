dbaf16315bae792532842ae141b8ae8ab89f2e25
adnan-0603 <adnan.chowdhury01@northsouth.edu>
2020-07-09 22:08:52 +0600

    adding Header.js and App.js File

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React  from 'react';
+import Homepage from './HomePage';
+import AboutPage from './AboutPage';
+import Header from "./common/Header";
+
+function App() {
+    function getPage(){
+    const  route  = window.location.pathname;
+    if( route === "/about") return <AboutPage />;
+    return <Homepage />;
+ }
+ return (<div className = "container-fluid">
+    <Header />
+    { getPage()}
+   </div>
+ );
+}
+
+export default App;
