996a22be465572ada03bc23b06e49d30785985e3
adnan-0603 <adnan.chowdhury01@northsouth.edu>
2020-07-12 14:36:57 +0600

    pops+State+LifecycleMethods+keys

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,22 @@
-import React  from 'react';
-import Homepage from './HomePage';
-import AboutPage from './AboutPage';
+import React from "react";
+import HomePage from "./HomePage";
+import AboutPage from "./AboutPage";
 import Header from "./common/Header";
-import CoursesPage from './CoursesPage';
+import CoursesPage from "./CoursesPage";
 
 function App() {
-    function getPage(){
-    const  route  = window.location.pathname;
-    if( route === "/courses") return <CoursesPage />;
-    if( route === "/about") return <AboutPage />;
-    return <Homepage />;
- }
- return (<div className = "container-fluid">
-    <Header />
-    { getPage()}
-   </div>
- );
-  
+  function getPage() {
+    const route = window.location.pathname;
+    if (route === "/courses") return <CoursesPage />;
+    if (route === "/about") return <AboutPage />;
+    return <HomePage />;
+  }
+  return (
+    <div className="container-fluid">
+      <Header />
+      {getPage()}
+    </div>
+  );
 }
 
 export default App;
--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,12 +1,12 @@
-import React from  "react";
+import React from "react";
 
 function Header() {
-    return (
-        <nav>
-            <a href ="/"> Home </a> | <a href = "/about">About </a> |{" "}
-            <a href = "/courses">Courses</a>
-        </nav>
-    );
+  return (
+    <nav>
+      <a href="/">Home</a> | <a href="/courses">Courses</a> |{" "}
+      <a href="/about">About</a>
+    </nav>
+  );
 }
 
 export default Header;
